b23709dbad88bf032d8c6684ed5816ab29bba155
georgethedeveloper77 <georgekarani77@gmail.com>
2021-06-25 12:41:01 +0300

    C# First Program
    Signed-off-by: georgethedeveloper77 <georgekarani77@gmail.com>

---
+++ C# First Program/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace C__Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
